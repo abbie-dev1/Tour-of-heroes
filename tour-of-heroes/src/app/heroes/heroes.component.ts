8d912ad565d39d0653173b8a25f391f935dc29a8
Abbie <79112224+AbbieS2175@users.noreply.github.com>
2021-08-11 15:56:48 +0200

    creatingFeature

--- tour-of-heroes/src/app/heroes/heroes.component.ts
+++ tour-of-heroes/src/app/heroes/heroes.component.ts
@@ -7,26 +7,25 @@ import { HEROES } from '../mock-heroes';
   templateUrl: './heroes.component.html',
   styleUrls: ['./heroes.component.css']
 })
-export class HeroesComponent implements OnInit {
+export class HeroesComponent implements OnInit 
+{
+    heroes = HEROES;
+    hero: Hero = 
+    {
+      id: 1,
+      name: 'Windstorm'
+    };
 
-  heroes = HEROES;
-  hero: Hero = 
-  {
-    id: 1,
-    name: 'Windstorm'
-  };
+    selectedHero?: Hero;
+    onSelect(hero: Hero): void 
+    {
+      this.selectedHero = hero;
+    }
 
-  selectedHero?: Hero;
-  onSelect(hero: Hero): void 
-  {
-    this.selectedHero = hero;
-  }
+    constructor() { }
 
-  constructor() { }
-
-  ngOnInit(): void 
-  {
-    
-  }
-
+    ngOnInit(): void 
+    {
+      
+    }
 }
